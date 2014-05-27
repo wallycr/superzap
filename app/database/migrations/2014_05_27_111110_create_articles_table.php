91e3cf1a3792e2c5db34afb069395376ad5cc19c
Walter Walker <walkerpottinger@gmail.com>
2014-05-27 06:26:15 -0600

    Archivos Iniciales

---
+++ app/database/migrations/2014_05_27_111110_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+	        $table->increments('id');
+	        $table->string('name');
+	        $table->text('description');
+	        $table->float('price');
+	        $table->integer('total_in_shelf');
+	        $table->integer('total_in_vault');
+	        $table->integer('store_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('articles', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
